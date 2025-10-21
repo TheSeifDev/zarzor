5785c872de78ec41a15f474cff6c4cb8437897af
Seif Ayman <167328383+S3fpl@users.noreply.github.com>
2025-10-21 22:10:33 +0300

    structure : About Section

--- components/Home/About/About.tsx
+++ components/Home/About/About.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Image from "next/image";
 
-const About = () => {
+export default function About() {
   return (
-    <section>About</section>
-  )
-}
+    <section className="grid grid-cols-1 md:grid-cols-2 min-h-screen gap-x-8">
+      {/* Left side - Image */}
+      <div className="relative w-full h-[400px] md:h-auto">
+        <Image
+          src="/images/about.jpg"
+          alt="Photographer"
+          fill
+          className="object-cover border"
+          priority
+        />
+      </div>
 
-export default About
+      {/* Right side - Text */}
+      <div className="flex flex-col justify-start p-10">
+        <h2 className="text-5xl md:text-6xl font-light tracking-tight mb-16">
+          ABOUT <br /> <span className="font-thin">ME</span>
+        </h2>
+        <p className="text-gray-700 text-sm leading-relaxed max-w-md ml-56">
+          I started my career in landscape photography and then eventually moved
+          into architectural photography. I was amazed at the patterns that
+          human hands could produce. I made it my mission to show more of it to
+          the world.
+        </p>
+      </div>
+    </section>
+  );
+}
