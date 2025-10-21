952c593cff48be32ded6fe44c704fd384f6fb3ea
Seif Ayman <167328383+S3fpl@users.noreply.github.com>
2025-10-21 21:52:39 +0300

    add : hero section

--- components/Home/Experience/Expreience.tsx
+++ components/Home/Experience/Expreience.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Expreience = () => {
   return (
-    <div>Expreience</div>
+    <section>Expreience</section>
   )
 }
 
--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="grid grid-cols-2 h-screen">
+      {/* Left side - Images */}
+      <div className="flex flex-col gap-4 p-4">
+        <div className="relative w-full h-1/2 min-h-[200px]">
+          <Image
+            src="/images/landscape1.jpg"
+            alt="Landscape 1"
+            fill
+            className="object-cover rounded- border"
+            priority
+          />
+        </div>
+        <div className="relative w-full h-1/2 min-h-[200px]">
+          <Image
+            src="/images/landscape2.jpg"
+            alt="Landscape 2"
+            fill
+            className="object-cover rounded- border"
+          />
+        </div>
+      </div>
 
-export default Hero
+      {/* Right side - Text content */}
+      <div className="flex flex-col justify-between p-8">
+        <h1 className="text-5xl font-bold leading-tight">
+          Discover the Beauty of Nature
+        </h1>
+
+        <div className="flex items-center justify-between text-sm">
+          <div>
+          <p className="">Ahmed Eid</p>
+          <p className="">Photographer & App developer</p>
+          </div>
+          <a href="#" className="text-gray-400 underline font-bold uppercase hover:cursor-pointer">
+            Work with me
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
